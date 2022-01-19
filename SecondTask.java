de79347478f4a44cd3f2562614419722ef8ae3dc
Fredy Ávila <fredyr@hotmail.com>
2022-01-18 19:47:37 -0600

    Final solution proposal

--- SecondTask.java
+++ SecondTask.java
@@ -1,16 +1,19 @@
 
 import java.util.ArrayList;
+import java.util.ArrayList;
+import java.util.LinkedList;
 
 public class SecondTask {
 
 	private static void findVowels(String str,int position) {
-		if(position == 0) return;
-		char[] stringChars= str.toCharArray();
+		if(position == 0) return; //Validate positions 
+		char[] stringChars= str.toCharArray(); //Convert the string to chars array
 		System.out.println("Original String: "+str);
 		System.out.println();
 		System.out.println("Vowels every "+ position+ " positions in string:");
-		for(int i=position-1;i<stringChars.length;i+=position) {
+		for(int i=position-1;i<stringChars.length;i+=position) { //Iterate through string array , starting at the position and incrementing i with the position given
 			if(stringChars[i] == 'a' || stringChars[i] == 'e' || stringChars[i]=='i' || stringChars[i]=='o' || stringChars[i]=='u') {
+				//If array at position i is a vowel print current character
 				System.out.print(stringChars[i]+" ");
 			}
 		}
@@ -18,48 +21,42 @@ public class SecondTask {
 	}
 	
 	private static int findSoldierPosition(int n) {
-		if(n<=0) {
-			return -1;
+		if(n<=0) return -1; //Validate positive numbers for n
+		LinkedList<Integer> ll = new LinkedList<Integer>(); //List to store the soldiers
+		for(int i=1;i<=n;i++) { //Add the numbers representing the soldiers to the list
+			ll.add(i);
 		}
-		//See if n is power of 2 because all power of 2 numbers the winner will always be first person
-		if((n!=0) && ((n & (n-1))) == 0){
-			 return 1;
-		 }
-		//Get the highest possible power of 2 number
-		int highestPwrOf2Number = 0;
-		for(int i=n;i>=1;i--) {
-			if((i & (i-1)) == 0) {
-				highestPwrOf2Number = i;
-				break;
+		int index=0; //Define index
+		while(ll.size() != 1) { //While there's more than 1 element on the list continue
+			if(index > ll.size()-1) { //If the index is greater than the list size then reset to 0
+				//This represents that a lap has been completed in the circle
+				index=0;	
 			}
+			else {
+				if(index+1 == ll.size()) { //If our current position is the same as the size of the list containing the soldiers that are alive
+					//then kill the one at the beginning
+					ll.removeFirst();
+				}
+				else {
+					ll.remove(index+1); //Kill the soldier to my right
+				}
+				index++; //Increase the index to move to next alive soldier
+			}
+			
 		}
-		//Store all soldiers in a list
-		ArrayList<Integer> soldiers = new ArrayList<Integer>();
-		for(int i=1;i<=n;i++) {
-			soldiers.add(i);
-		}
-		//Get the remainder of number of soldiers minus highest possible power of 2 number
-		int remainder = n-highestPwrOf2Number;
-		int indexLastSoldier = 0;
-		//Iterate from 0 to the remainder
-		for(int i=0;i<remainder;i++) {
-			soldiers.remove(i+1); //Kill the soldier next to me 
-			indexLastSoldier = i+1; //Save the index
-		}
-		return soldiers.get(indexLastSoldier); //Return the soldier at the index saved
-		
+		//At this point only one soldier is left so return the soldier that is alive
+		return ll.getFirst();
 	}
-	
 	
 	public static void main(String[] args) {
 		String inputString = new String("happy new year 2022");
 		findVowels(inputString,3);
-		int numberOfSoldiers = 9;
+		int numberOfSoldiers = 41;
 		int result = findSoldierPosition(numberOfSoldiers);
 		if(result==-1){
-			System.out.println("Please enter a positive number greather than 0");
+			System.out.println("Number of soldiers should be greater than 0");
 			return;
 		}
-		System.out.println("With "+ numberOfSoldiers + " soldiers, you need to sit at index "+result+ " to live");
+		System.out.println("With "+ numberOfSoldiers + " soldiers, you need to sit at position "+result+ " to live");
 	}
 }
